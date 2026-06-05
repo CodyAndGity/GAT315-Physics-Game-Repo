391b07a7ba0be7f068a3fe897a9833713ab91fa4
CodyAndGity <mdavis@student.neumont.edu>
2026-06-04 23:39:40 -0600

    Added music and death sound

--- Assets/PolyBridge/Scripts/DeadlyZone.cs
+++ Assets/PolyBridge/Scripts/DeadlyZone.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class DeadlyZone : MonoBehaviour
 {
+    [SerializeField] public GameObject deathSound;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             GameManager.Instance.TriggerPlayerRespawn();
+            GameObject.Instantiate(deathSound);
             print("Player entered deadly zone, triggering respawn.");
         }
     }
